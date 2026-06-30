d184856a84277899b8564e6ab75ed43073f609bf
jpadgett30 <jpadgett30@ivytech.edu>
2026-06-30 00:02:00 -0400

    testing

--- script.js
+++
@@ -1,9 +0,0 @@
-document.querySelector("#alert-btn").addEventListener("click", function() {
-    const alert = document.querySelector("#viewport");
-    alert.classList.remove("hidden");
-});
-
-document.querySelector(".close").addEventListener("click", function(){
-    const alert = document.querySelector("#viewport");
-    alert.classList.add("hidden");
-});
