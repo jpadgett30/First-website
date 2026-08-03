402c3b2070cc1a1c4b5b112c19a66a76be88c9f5
jpadgett30 <jpadgett30@ivytech.edu>
2026-08-02 23:58:49 -0400

    initialcommit

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
-const textElemnet = document.getElementById("Section1");
 
-textElemnet.addEventListener("click", function() {
-    window.location.href = "contact.html";
-});
+window.addEventListener("DOMContentLoaded", function () {
+    let contactForm = document.querySelector("#contactForm");
+    let formMessage = document.querySelector("#formMessage");
+
+    contactForm.addEventListener("submit", function () {
+        event.preventDefault();
+
+        let name = document.querySelector("#name").value.trim();
+        let email = document.querySelector("#email").value.trim();
+        let emailTest = document.querySelector("#email")
+        let subject = document.querySelector("#subject").value;
+        let message = document.querySelector("#message").value.trim();
+
+        if (name === "") {
+            formMessage.textContent = "Please enter a name";
+            formMessage.className = "errorMessage";
+        }
+        else if (email === "" ) {
+            formMessage.textContent = "Please enter an email";
+            formMessage.className = "errorMessage";
+        }
+        else if (!emailTest.checkValidity()) {
+            formMessage.textContent = "Please enter a valid email";
+            formMessage.className = "errorMessage";
+        }
+        else if (subject === "") {
+            formMessage.textContent = "Please choose a subject";
+            formMessage.className = "errorMessage";
+        }
+        else if (message === "" ) {
+            formMessage.textContent = "Please enter a message";
+            formMessage.className = "errorMessage";
+        }
+        else {
+            formMessage.textContent = "Thank you for your message."
+            formMessage.className = "successMessage"
+        }
+    });
+
+
+    
+    
+})
